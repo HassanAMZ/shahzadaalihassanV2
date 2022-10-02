7a4eee3abadd5fc74ca739edd705425926980834
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-10-03 04:27:42 +0500

    Fixed MDX componenets

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -13,42 +13,50 @@ export const MDXComponents = {
   a: CustomLink,
   pre: Pre,
   BlogNewsletterForm: BlogNewsletterForm,
-  ol: (props) => {
-    return (
-      <ol type="1" className="list-decimal list-outside my-1 pl-[40px] mx-0">
-        {props.children}
-      </ol>
-    )
+  ol: ({ ...rest }) => {
+    return <ol type="1" className="list-decimal list-outside my-1 pl-[40px] mx-0" {...rest} />
   },
-  code: (props) => {
-    return <code className="whitespace-pre-wrap py-2 break-words">{props.children}</code>
+  code: ({ ...rest }) => {
+    return <code className="whitespace-pre-wrap py-2 break-words" {...rest} />
   },
   h1: (props) => {
     return (
-      <h1 className=" py-2 text-5xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-6xl lg:text-7xl dark:text-white">
+      <h1
+        className=" py-2 text-5xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-6xl lg:text-7xl dark:text-white"
+        {...props}
+      >
         {props.children}
       </h1>
     )
   },
   h2: (props) => {
     return (
-      <h2 className="py-2 text-4xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-5xl dark:text-white">
+      <h2
+        className="py-2 text-4xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-5xl dark:text-white"
+        {...props}
+      >
         {props.children}
       </h2>
     )
   },
   h3: (props) => {
     return (
-      <h3 className="py-2 text-3xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-4xl dark:text-white">
+      <h3
+        className="py-2 text-3xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-4xl dark:text-white"
+        {...props}
+      >
         {props.children}
       </h3>
     )
   },
   h4: (props) => {
     return (
-      <h3 className="py-2 text-2xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-3xl dark:text-white">
+      <h4
+        className="py-2 text-2xl font-bold capitalize tracking-tight leading-none text-gray-900 sm:text-3xl dark:text-white"
+        {...props}
+      >
         {props.children}
-      </h3>
+      </h4>
     )
   },
   wrapper: ({ components, layout, ...rest }) => {
