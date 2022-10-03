e623e2bf7370fb6e1341f213870645e8d24110e6
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-10-03 23:23:13 +0500

    Upadted 00035

--- components/Image.js
+++ components/Image.js
@@ -2,7 +2,7 @@ import NextImage from 'next/image'
 
 // eslint-disable-next-line jsx-a11y/alt-text
 const Image = ({ ...rest }) => (
-  <div className="flex items-center justify-center py-2">
+  <div className="flex items-center w-full justify-center py-2">
     <NextImage {...rest} />
   </div>
 )
