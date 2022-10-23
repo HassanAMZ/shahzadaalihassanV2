e449d5941f70a3bf825724bf9a3a5d6f68077160
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-10-23 19:05:41 +0500

    Updated 404 Template

--- pages/404.js
+++ pages/404.js
@@ -10,12 +10,16 @@ export default function FourZeroFour() {
           <p className="text-2xl py-2 sm:px-16 sm:py-4 font-medium">
             Oops! The page you are looking for does not exist. It might have been moved or deleted.
           </p>
-          <button className="bg-gradient-to-r from-teal-400 to-teal-500 hover:from-teal-500 hover:to-white-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
-            <CustomLink href="/">HOME</CustomLink>
-          </button>
-          <button className="bg-gradient-to-r from-teal-400 to-teal-500 hover:from-teal-500 hover:to-teal-500 text-white font-semibold px-6 py-3 rounded-md">
-            <CustomLink href="/blog">Blog</CustomLink>
-          </button>
+          <CustomLink href="/">
+            <button className="bg-gradient-to-r from-teal-400 to-teal-500 hover:from-teal-500 hover:to-white-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
+              HOME
+            </button>
+          </CustomLink>
+          <CustomLink href="/blog">
+            <button className="bg-gradient-to-r from-teal-400 to-teal-500 hover:from-teal-500 hover:to-teal-500 text-white font-semibold px-6 py-3 rounded-md">
+              Blog
+            </button>
+          </CustomLink>
         </div>
       </div>
     </WebsiteLayout>
