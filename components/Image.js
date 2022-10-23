c864fdddb51666258542dda65e394bf0c347a054
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-10-23 11:54:57 +0500

    Added a new blog 00050

--- components/Image.js
+++ components/Image.js
@@ -2,7 +2,7 @@ import NextImage from 'next/image'
 
 // eslint-disable-next-line jsx-a11y/alt-text
 const Image = ({ ...rest }) => (
-  <div className="flex items-center border-black dark:border-white border-2 rounded-lg justify-center p-2">
+  <div className="flex my-2 items-center border-black dark:border-white border-2 rounded-lg justify-center p-2">
     <NextImage {...rest} />
   </div>
 )
